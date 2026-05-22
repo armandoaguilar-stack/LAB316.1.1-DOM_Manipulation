f6a4f81a9775cca0b3abb3fa5b848ac4889e0299
Armando Aguilar <armandinhobarca10@gmail.com>
2026-05-21 21:16:58 -0400

    Implement DOM manipulation for main element

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //DOM Manipulation Part 1
 
 // 1. Select and cache the <main> element in a variable named mainEl.
+const mainEl = document.querySelector('main');
 
 // Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
-
 // Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+mainEl.style.backgroundColor = 'var(--main-bg)'; // var() https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Cascading_variables/Using_custom_properties
 
 // Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>'; // h1 from index.html
 
 // Add a class of flex-ctr to mainEl.
-
 // Hint: Use the Element.classList API.
+mainEl.classList.add('flex-ctr'); // flex-ctr from styles.css
